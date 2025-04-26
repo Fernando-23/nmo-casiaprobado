a63da791f6326f3c7a4b5cf54fd2400ac0144f42
santti0 <scalizayamamani@frba.utn.edu.ar>
2025-04-26 23:25:35 +0000

    finalizando PCB

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"time"
 
 	cliente "github.com/sisoputnfrba/tp-2025-1c-Nombre-muy-original/utils/Cliente"
 	servidor "github.com/sisoputnfrba/tp-2025-1c-Nombre-muy-original/utils/Servidor"
@@ -11,7 +13,13 @@ import (
 
 func main() {
 
+	args := os.Args
+
+	fmt.Println("archivo de pseudocodigo ", args[1])
+	fmt.Println("tamaño de proceso", args[2])
+
 	fmt.Printf("Iniciando Kernel...")
+
 	config_kernel = iniciarConfiguracionKernel("config.json")
 	url := fmt.Sprintf("http://%s:%d/", config_kernel.Ip_kernel, config_kernel.Puerto_Memoria)
 
@@ -21,8 +29,14 @@ func main() {
 		slog.Info("Error al conectarse con memoria", req)
 	}
 
+	inicio := time.Now()
+	duracion := time.Since(inicio)
+
+	fmt.Println("tiempo: ", duracion)
+
 	//Iniciar servidor
 	mux := http.NewServeMux()
+	pid := 0
 
 	mux.HandleFunc("/paquetes", servidor.RecibirPaquetes)
 	mux.HandleFunc("/mensaje", servidor.RecibirMensaje)
@@ -36,4 +50,11 @@ func main() {
 	}
 
 	cliente.EnviarMensaje(config_kernel.Ip_memoria, config_kernel.Puerto_Memoria, "Hola soy modulo Kernel")
+
+	// Detener kERNEL para esperar las conexiones de CPU y despues iniciar la PLANIFICACION
+
+	detenerKernel()
+
+	iniciarPlanificadorLP(args[2], &pid)
+
 }
--- kernel/utils_kernel.go
+++ kernel/utils_kernel.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
 type ConfigKernel struct {
-	Ip_memoria              string `json:"ip_memory"`
-	Puerto_Memoria          int    `json:"port_memory"`
-	Algoritmo_Plani         string `json:"scheduler_algorithm"`
-	Ready_ingress_algorithm string `json:"ready_ingress_algorithm"`
-	Alfa                    int    `json:"alpha"`
-	Tiempo_Suspension       int    `json:"suspension_time"`
-	Log_leveL               string `json:"log_level"`
-	Puerto_Kernel           int    `json:"port_kernel"`
-	Ip_kernel               string `json:"ip_kernel"`
+	Ip_memoria              string  `json:"ip_memory"`
+	Puerto_Memoria          int     `json:"port_memory"`
+	Algoritmo_Plani         string  `json:"scheduler_algorithm"`
+	Ready_ingress_algorithm string  `json:"ready_ingress_algorithm"`
+	Alfa                    float64 `json:"alpha"`
+	Tiempo_Suspension       int     `json:"suspension_time"`
+	Log_leveL               string  `json:"log_level"`
+	Puerto_Kernel           int     `json:"port_kernel"`
+	Ip_kernel               string  `json:"ip_kernel"`
+}
+
+const cantEstados int = 6
+
+type Pcb struct {
+	Pid int
+	Pc  int
+	Me  [cantEstados]int     //Metricas de Estado
+	Mt  [cantEstados]float64 //Metricas de Tiempo
+
 }
 
 var config_kernel *ConfigKernel
+var cola_new []*Pcb
+var cola_ready []*Pcb
+var cola_block []*Pcb
+
+// var cola_susp_block []
+// var cola_susp_ready []
+
+// array de arrays que contenga a todas las colas
 
 func iniciarConfiguracionKernel(filePath string) *ConfigKernel {
 	var configuracion *ConfigKernel
@@ -32,4 +52,35 @@ func iniciarConfiguracionKernel(filePath string) *ConfigKernel {
 	jsonParser.Decode(&configuracion)
 
 	return configuracion
+}
+
+func detenerKernel() {
+
+	fmt.Println("Ingrese ENTER para empezar con la planificacion")
+
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	fmt.Println("Empezando con la planificacion")
+
+}
+func iniciarPlanificadorLP(tamanio string, pid *int) {
+
+	ingresarColaNew(pid)
+
+}
+
+func ingresarColaNew(pid *int) {
+	pcb := new(Pcb)
+	pcb.Pid = *pid
+	*pid++
+	pcb.Pc = 0
+	//inicio := time.Now()
+	cola_new = append(cola_new, pcb)
+
+}
+
+func modificarEstado(pcb *Pcb, pos int) {
+
+	pcb.Me[pos]++
+
 }
