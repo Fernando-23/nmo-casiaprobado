8857ebc2e4c17bd7c2680f5fecf26b0d25d4f742
santti0 <scalizayamamani@frba.utn.edu.ar>
2025-04-29 03:56:15 +0000

    IO casi finalizado

--- io/io.go
+++ io/io.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
 
 	cliente "github.com/sisoputnfrba/tp-2025-1c-Nombre-muy-original/utils/Cliente"
 )
 
 func main() {
+	args := os.Args
+	nombre_io := args[1]
+
 	fmt.Println("Iniciando I/O..")
 	config_IO = iniciarConfiguracionIO("config.json")
+
+	handshakeKernel(nombre_io)
+
+	//Iniciando servidor para peticiones I/O - Kernel
+	mux := http.NewServeMux()
+	mux.HandleFunc("/recibir_peticion", atenderPeticion)
+
+	slog.Debug("Iniciando servidor para peticiones I/O - Kernel")
+
+	socket:= fmt.Sprintf(":%d", config_IO.Puerto_io)
+	api_peticiones_kernel := http.ListenAndServe(socket, mux)
+	if api_peticiones_kernel != nil {
+		panic("Error al iniciar el servidor")
+	}
 
 	cliente.EnviarMensaje(config_IO.Ip_kernel, config_IO.Puerto_kernel, "Hola soy modulo IO")
 
 }
+
--- io/utils_io.go
+++ io/utils_io.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
 type ConfigIO struct {
@@ -12,6 +16,17 @@ type ConfigIO struct {
 	Puerto_io     int    `json:"port_io"`
 	Ip_io         string `json:"ip_io"`
 	Log_level     string `json:"log_level"`
+}
+
+type HandshakeRequest struct {
+	NombreIO string `json:"nombre_io"`
+	PuertoIO int    `json:"puerto_io`
+	IpIO     string `json:"ip_io"`
+}
+
+type Peticion struct {
+	Pid    int `json:"pid"`
+	Tiempo int `json:"tiempo"`
 }
 
 var config_IO *ConfigIO
@@ -28,4 +43,45 @@ func iniciarConfiguracionIO(filePath string) *ConfigIO {
 	jsonParser.Decode(&configuracion)
 
 	return configuracion
+}
+
+func handshakeKernel(nombre string) {
+	peticion := HandshakeRequest{
+		NombreIO: nombre,
+		PuertoIO: (*config_IO).Puerto_io,
+		IpIO:     (*config_IO).Ip_io,
+	}
+
+	url := fmt.Sprintf("http://%s:%d/", (*config_IO).Ip_io, (*config_IO).Puerto_kernel)
+	body, err := json.Marshal(peticion)
+	if err != nil {
+		log.Printf("Error codificando mensaje: %s", err.Error())
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Println("Error enviando datos", resp)
+		return
+	}
+
+	log.Println("Hanshake realizado correctamente!")
+}
+
+func atenderPeticion(w http.ResponseWriter, r *http.Request) {
+	var peticion_recibida Peticion
+	err := json.NewDecoder(r.Body).Decode(&peticion_recibida)
+
+	if err != nil {
+		log.Printf("Error al decodificar mensaje: %s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error al decodificar mensaje"))
+		return
+	}
+
+	time.Sleep(time.Duration(peticion_recibida.Tiempo) * time.Millisecond )
+	w.WriteHeader(http.StatusOK)
+	finalizoIO := fmt.Sprintf("%d",peticion_recibida.Pid)
+	w.Write([]byte(finalizoIO))
+
 }
